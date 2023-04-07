8e7b3bc9753e45a35c7485100e69fa9da4f41534
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-07 15:58:37 +0500

    lesson one

---
+++ Lesson_1/main.py
@@ -0,0 +1,110 @@
+# n = 'sdfd\'sfsd'
+# print(n)
+
+# n = 5
+# print(n)
+# print(n)
+# print(n)
+# print(n)
+
+'''
+print(n, n)
+print(n, n)
+'''
+
+'''
+a = 5
+b = 5.89
+c = 'hello'
+
+print(a, ' - ', b, ' - ', c)
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))
+'''
+
+
+# print('Type the first number')
+# a = int(input())
+# b = int(input('Type the second number: '))
+
+# print(f"{a} + {b} = {a+b}")
+
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+
+# a = 5.89956
+# b = 6.556531
+# print(round(a*b, 3))
+
+# a = 1 > 4
+# print(a)
+
+# a = 1 < 4 and 5 > 2
+# print(a)
+
+# a = 1 == 2
+# print(a)
+
+# a = 1 != 2
+# print(a)
+
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+""" username = input('Type your name: ')
+if username == 'Masha':
+    print('Hooray, it\'s Masha')
+elif username == 'Marina':
+    print('I\'ve been waiting for ya, Marina')
+elif username == 'Mark':
+    print('Hello, Mark')
+else:
+    print(f'Hello there, {username}') """
+
+
+""" i = 0
+while i < 5:
+    # if i == 3:
+    #     break
+    i = i + 1
+else:
+    print('I guess')
+    print('it\'s enough')
+print(i) """
+
+""" n = int(input())
+flag = True
+i = 2
+while flag:
+    if n % i == 0:
+        flag = False
+        print(i)
+    elif i > n // 2:
+        print(n)
+        flag = False
+    i += 1  """
+    
+
+""" line = ''
+for i in range(5):
+    line = ''
+    for j in range(5):
+        line += '*'
+    print(line) """
+    
+""" text = 'Eat sOmE more Of ThEsE sOfT FrEnCh bUnS'
+print(len(text))
+print('more' in text)
+print(text.lower())
+print(text.upper())
+print(text.replace('more', 'MoRe')) """
