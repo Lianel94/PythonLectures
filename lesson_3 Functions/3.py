ba639981b235f3dfeb8151dc66d3ccdd2b4160d3
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-22 22:28:14 +0500

    sorting

--- lesson_3 Functions/3.py
+++ lesson_3 Functions/3.py
@@ -34,3 +34,46 @@ list1 = []
 for i in range(1, 10):
     list1.append(fib(i))
 print(list1)
+
+# Quick Sorting
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+    less = [i for i in array[1:] if i  <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+print(quick_sort([14, 5, 9, 58, 7, 5, 2]))
+
+# Combining Sorting
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+list2 = [1,5,10,6,9,8,7,2,1,55,2,4]
+merge_sort(list2)
+print(list2)
