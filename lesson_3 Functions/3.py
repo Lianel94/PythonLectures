e6a48fd5c08df1307afd02f00cae8df88694407b
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-22 21:57:46 +0500

    new files for working with modules

--- lesson_3 Functions/3.py
+++ lesson_3 Functions/3.py
@@ -1,7 +1,24 @@
-def sum_numbers(n):
+def sum_numbers(n, y = 'Hello'):
+    print(y)
     summa = 0
     for i in range(1, n + 1):
         summa += i
-    print(summa)
+    return summa
+    print('stop') #return finishes the function
 
-sum_numbers(5)
+""" a = sum_numbers(5)
+print(a) """
+print(sum_numbers(5, 'qwert'))
+
+
+def sum_str(*args):
+    res = ''
+    for i in args:
+        res += i
+    return res
+
+print(sum_str('q', 'e', 'l'))
+print(sum_str('q', 'e', 'l', 'f', 'r'))
+print(sum_str(1, 8, 9)) #error, int not str
+
+# MODULES
