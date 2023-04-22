edd2af4061f5afe95b7bec58709a717959b6cfbd
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-22 22:00:14 +0500

    new files for working with modules additions

--- lesson_3 Functions/main.py
+++ lesson_3 Functions/main.py
@@ -1,3 +1,6 @@
-import modul1
+""" import modul1 as m1
 
-print(modul1.max1(5, 9))
+print(m1.max1(5, 9)) """
+
+from modul1 import max1 #from modul1 import * - import all the functions from modul 1
+print(max1(10, 9))
